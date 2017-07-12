eced400d76be5d56d098a30209a811007111efab
Moshe Solomon <udeleng@gmail.com>
2017-07-11 22:38:33 -0700

    Added Meteor accounts-password package and added calls to create user and login with password.

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { Meteor } from 'meteor/meteor';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Meteor.loginWithPassword({email}, password, (err) => {
+      console.log('Login callback', err);
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Login to Short Lnk</h1>
+        <h1>Short Lnk</h1>
 
-        Login form here
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-        <Link to="/signup">Have an account?</Link>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name="email" placeholder="Email"/>
+          <input type="password" ref="password" name="password" placeholder="Password"/>
+          <button>Login</button>
+        </form>
+
+        <Link to="/signup">Need an account?</Link>
       </div>
     );
   }
