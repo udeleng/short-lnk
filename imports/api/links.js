6a78ca4ccc410d05e2e68ef56986c199ad8afdc0
Moshe Solomon <udeleng@gmail.com>
2017-08-18 23:07:44 -0700

    Configured a custom simple schema error, added URL validation

--- imports/api/links.js
+++ imports/api/links.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
+import SimpleSchema from 'simpl-schema';
+
 export const Links = new Mongo.Collection('links');
 
 if (Meteor.isServer) {
@@ -14,6 +16,14 @@ Meteor.methods({
         if (!this.userId) {
             throw new Meteor.Error('not-authorized');
         }
+
+        new SimpleSchema({
+        url: {
+            type: String,
+            label: 'Your link',
+            regEx: SimpleSchema.RegEx.Url
+        }
+        }).validate({url});
 
         Links.insert({
             url,
--- imports/api/users.js
+++ imports/api/users.js
@@ -3,18 +3,14 @@ import { Accounts } from 'meteor/accounts-base';
 import SimpleSchema from 'simpl-schema';
 
 Accounts.validateNewUser((user) => {
-    const email  =user.emails[0].address;
+    const email = user.emails[0].address;
 
-    try {
-        new SimpleSchema({
-        email: {
-            type: String,
-            regEx: SimpleSchema.RegEx.Email
-        }
-        }).validate({email});
-    } catch (e) {
-        throw new Meteor.Error(400, e.message);
+    new SimpleSchema({
+    email: {
+        type: String,
+        regEx: SimpleSchema.RegEx.Email
     }
+    }).validate({email});
 
     return true;
 });
