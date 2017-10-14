71c5c8a331068c5a5c07cdcb7de54ebf9c0cfc7f
Moshe Solomon <moshes@aptare.net>
2017-10-13 17:36:10 -0700

    Moved links header and add link form into separate components; converted several components into stateless components

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,37 +1,16 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
 
 import { Links } from '../api/links';
 import LinksList from './LinksList';
+import PrivateHeader from './PrivateHeader';
+import AddLink from './AddLink';
 
-export default class Link extends React.Component {
-    logout() {
-        Accounts.logout();
-    }
-
-    onSubmit(e) {
-        const url = this.refs.url.value.trim();
-        e.preventDefault();
-        if (url) {
-            Meteor.call('links.insert', url);
-            this.refs.url.value = '';
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <h2>Your Links</h2>
-                <button onClick={this.logout.bind(this)}>Logout</button>
-                <LinksList></LinksList>
-                <p>Add Link</p>
-                <form onSubmit={this.onSubmit.bind(this)}>
-                    <input type="text" ref="url" placeholder="URL"/>
-                    <button>Add Link</button>
-                </form>
-            </div>
-        )
-    }
+export default () => {
+    return (
+        <div>
+            <PrivateHeader title="Your Links"/>
+            <LinksList></LinksList>
+            <AddLink />
+        </div>
+    )
 }
-
--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-export default class NotFound extends React.Component {
-  render() {
-    return <p>NotFound component here</p>
-  }
-}
+export default () => {
+  return <p>NotFound component here</p>
+};
