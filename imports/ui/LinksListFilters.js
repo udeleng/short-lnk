38eae9fceba690cbab1a94b680198fc38f736476
Moshe Solomon <moshes@aptare.net>
2017-10-17 20:47:33 -0700

    made add link input to bind to state variable 'url' and the 'show visible' checkbox bound to 'showVisible' state variable

--- imports/ui/LinksListFilters.js
+++ imports/ui/LinksListFilters.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { Tracker } from 'meteor/tracker';
 
-export default () => {
-    return (
-        <div>
-            <label>
-                <input type="checkbox" onChange={(e) => {
-                    Session.set('showVisible', e.target.checked);
-                }} />
-                Show hidden links
-            </label>
-        </div>
-    )
+export default class LinksListFilters extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showVisible: true
+        }
+    }
+
+    componentDidMount() {
+        this.tracker = Tracker.autorun(() => {
+            this.setState({
+                showVisible: Session.get('showVisible')
+            });
+        });
+    }
+
+    componentWillUnmout() {
+        this.tracker.stop();
+    }
+
+    render() {
+        return (
+            <div>
+                <label>
+                    <input type="checkbox" checked={!this.state.showVisible} onChange={(e) => {
+                        Session.set('showVisible', !e.target.checked);
+                    }} />
+                    Show hidden links
+                </label>
+            </div>
+        )
+    }
 }
