9e8e316403d005c37c8698bc578a3b336678b57b
Moshe Solomon <moshes@aptare.net>
2017-10-18 21:37:05 -0700

    formatted analytics messages using moment.js

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import Clipboard from 'clipboard';
+import moment from 'moment';
 
 export default class LinksListItem extends React.Component {
     constructor(props) {
@@ -21,13 +22,23 @@ export default class LinksListItem extends React.Component {
     componentWillUnmout() {
         this.clipboard.destroy();
     }
+    renderStats() {
+        const visitMessage = this.props.visitedCount === 1 ? 'visit' : 'visits';
+        let visitedMessage = null;
+
+        if (typeof this.props.lastVisitedAt === 'number') {
+            visitedMessage = `(visited ${moment(this.props.lastVisitedAt).fromNow()})`
+        }
+
+        return <p>{this.props.visitedCount} {visitMessage} {visitedMessage}</p>;
+    }
     render() {
         return (
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
                 <p>{this.props.visible.toString()}</p>
-                <p>{this.props.visitedCount} - {this.props.lastVisitedAt}</p>
+                <p>{this.renderStats()}</p>
                 <button ref="copy" data-clipboard-text={this.props.shortUrl}>
                     {this.state.justCopied ? 'Copied' : 'Copy'}
                 </button>
