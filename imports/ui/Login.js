128cd63daa6a437206b5f683bee75d659fd03106
Moshe Solomon <moshes@aptare.net>
2017-10-19 23:08:32 -0700

    defined styles for box and box overly for login and signup pages

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -27,18 +27,20 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Short Lnk</h1>
+      <div className="boxed-view">
+        <div className="boxed-view__box">
+          <h1>Short Lnk</h1>
 
-        {this.state.error ? <p>{this.state.error}</p> : undefined}
+          {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-        <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="email" ref="email" name="email" placeholder="Email"/>
-          <input type="password" ref="password" name="password" placeholder="Password"/>
-          <button>Login</button>
-        </form>
+          <form onSubmit={this.onSubmit.bind(this)}>
+            <input type="email" ref="email" name="email" placeholder="Email"/>
+            <input type="password" ref="password" name="password" placeholder="Password"/>
+            <button>Login</button>
+          </form>
 
-        <Link to="/signup">Need an account?</Link>
+          <Link to="/signup">Need an account?</Link>
+        </div>
       </div>
     );
   }
