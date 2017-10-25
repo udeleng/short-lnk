7782c177da72f83bafd299e3ab3fb0a59b5d375f
Moshe Solomon <moshes@aptare.net>
2017-10-24 22:20:56 -0700

    Added styles for form, button, and anchors

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -36,7 +36,7 @@ export default class Login extends React.Component {
           <form onSubmit={this.onSubmit.bind(this)} noValidate className="boxed-view__form">
             <input type="email" ref="email" name="email" placeholder="Email"/>
             <input type="password" ref="password" name="password" placeholder="Password"/>
-            <button>Login</button>
+            <button className="button">Login</button>
           </form>
 
           <Link to="/signup">Need an account?</Link>
--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -7,7 +7,7 @@ export default () => {
       <div className="boxed-view__box">
         <h1>Page Not Found</h1>
         <p>We're unable to find that page.</p>
-        <Link to="/">Head Home</Link>
+        <Link to="/" className="button button--link">Head Home</Link>
       </div>
     </div>
   )
