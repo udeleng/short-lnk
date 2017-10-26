69c399771ccfd0275ff7519f203e4ff6c532ab00
Moshe Solomon <moshes@aptare.net>
2017-10-25 23:10:09 -0700

    Styles main links content container and 'show hidden' checkbox

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -9,9 +9,11 @@ export default () => {
     return (
         <div>
             <PrivateHeader title="Your Links"/>
-            <LinksListFilters />
-            <AddLink />
-            <LinksList></LinksList>
+            <div className="page-content">
+                <LinksListFilters />
+                <AddLink />
+                <LinksList></LinksList>
+            </div>
         </div>
     )
 }
